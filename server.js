713e1b9bc68f96490f6c8b278720c4d90f895e6e
Wilmer Rivera <wilmerbaby@gmail.com>
2022-04-09 22:08:37 -0400

    test 4

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(logger("dev"));
 
 if(process.env.NODE_ENV === "production"){
   app.use(express.static("client/build"));
-}
+};
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/mac-tacosDB",{
   useNewUrlParser: true,
