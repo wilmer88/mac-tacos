5498b269f61faa93b1064efc97218c447ada8a59
Wilmer Rivera <wilmerbaby@gmail.com>
2022-04-09 20:49:47 -0400

    added if proces.env on server.js file

--- server.js
+++ server.js
@@ -13,7 +13,11 @@ app.use(express.json());
 app.use(routes);
 
 app.use(logger("dev"));
-app.use(express.static("client/build"));
+
+if(process.env.NODE_ENV === "production"){
+  app.use(express.static("client/build"));
+}
+
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/mac-tacosDB",{
   useNewUrlParser: true,
   // useUnifiedTopology: true,
